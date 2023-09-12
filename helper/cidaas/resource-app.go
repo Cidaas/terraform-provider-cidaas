1351b4dd3aacac9165f4559ef033e514db66a8d5
Tujit Bora <tujit.bora@widas.in>
2023-09-12 11:47:55 +0530

    fix(): type of additional token payload updated to fix json unmashal issue

--- helper/cidaas/resource-app.go
+++ helper/cidaas/resource-app.go
@@ -13,36 +13,35 @@ type AppResponse struct {
 }
 
 type AppConfig struct {
-	ClientId                     string      `json:"client_id,omitempty"`
-	ClientName                   string      `json:"client_name,omitempty"`
-	ClientDisplayName            string      `json:"client_display_name,omitempty"`
-	TemplateGroupId              string      `json:"template_group_id,omitempty"`
-	HostedPageGroup              string      `json:"hosted_page_group,omitempty"`
-	ClientType                   string      `json:"client_type,omitempty"`
-	AllowLoginWith               []string    `json:"allow_login_with,omitempty"`
-	AutoLoginAfterRegister       bool        `json:"auto_login_after_register,omitempty"`
-	EnablePasswordlessAuth       bool        `json:"enable_passwordless_auth,omitempty"`
-	RegisterWithLoginInformation bool        `json:"register_with_login_information,omitempty"`
-	EnableDeduplication          bool        `json:"enable_deduplication,omitempty"`
-	AllowDisposableEmail         bool        `json:"allow_disposable_email,omitempty"`
-	ValidatePhoneNumber          bool        `json:"validate_phone_number,omitempty"`
-	FdsEnabled                   bool        `json:"fds_enabled,omitempty"`
-	CompanyName                  string      `json:"company_name,omitempty"`
-	CompanyAddress               string      `json:"company_address,omitempty"`
-	CompanyWebsite               string      `json:"company_website,omitempty"`
-	AllowedScopes                []string    `json:"allowed_scopes,omitempty"`
-	ResponseTypes                []string    `json:"response_types,omitempty"`
-	LoginProviders               []string    `json:"login_providers,omitempty"`
-	AdditionalAccessTokenPayload string      `json:"additional_access_token_payload,omitempty"`
-	GrantTypes                   []string    `json:"grant_types,omitempty"`
-	RequiredFields               []string    `json:"required_fields,omitempty"`
-	IsHybridApp                  bool        `json:"is_hybrid_app,omitempty"`
-	RedirectURIS                 []string    `json:"redirect_uris,omitempty"`
-	AllowedLogoutUrls            []string    `json:"allowed_logout_urls,omitempty"`
-	AllowedWebOrigins            []string    `json:"allowed_web_origins,omitempty"`
-	AllowedOrigins               []string    `json:"allowed_origins,omitempty"`
-	MobileSettings               interface{} `json:"mobile_settings,omitempty"`
-
+	ClientId                      string      `json:"client_id,omitempty"`
+	ClientName                    string      `json:"client_name,omitempty"`
+	ClientDisplayName             string      `json:"client_display_name,omitempty"`
+	TemplateGroupId               string      `json:"template_group_id,omitempty"`
+	HostedPageGroup               string      `json:"hosted_page_group,omitempty"`
+	ClientType                    string      `json:"client_type,omitempty"`
+	AllowLoginWith                []string    `json:"allow_login_with,omitempty"`
+	AutoLoginAfterRegister        bool        `json:"auto_login_after_register,omitempty"`
+	EnablePasswordlessAuth        bool        `json:"enable_passwordless_auth,omitempty"`
+	RegisterWithLoginInformation  bool        `json:"register_with_login_information,omitempty"`
+	EnableDeduplication           bool        `json:"enable_deduplication,omitempty"`
+	AllowDisposableEmail          bool        `json:"allow_disposable_email,omitempty"`
+	ValidatePhoneNumber           bool        `json:"validate_phone_number,omitempty"`
+	FdsEnabled                    bool        `json:"fds_enabled,omitempty"`
+	CompanyName                   string      `json:"company_name,omitempty"`
+	CompanyAddress                string      `json:"company_address,omitempty"`
+	CompanyWebsite                string      `json:"company_website,omitempty"`
+	AllowedScopes                 []string    `json:"allowed_scopes,omitempty"`
+	ResponseTypes                 []string    `json:"response_types,omitempty"`
+	LoginProviders                []string    `json:"login_providers,omitempty"`
+	AdditionalAccessTokenPayload  []string    `json:"additional_access_token_payload,omitempty"`
+	GrantTypes                    []string    `json:"grant_types,omitempty"`
+	RequiredFields                []string    `json:"required_fields,omitempty"`
+	IsHybridApp                   bool        `json:"is_hybrid_app,omitempty"`
+	RedirectURIS                  []string    `json:"redirect_uris,omitempty"`
+	AllowedLogoutUrls             []string    `json:"allowed_logout_urls,omitempty"`
+	AllowedWebOrigins             []string    `json:"allowed_web_origins,omitempty"`
+	AllowedOrigins                []string    `json:"allowed_origins,omitempty"`
+	MobileSettings                interface{} `json:"mobile_settings,omitempty"`
 	AccentColor                   string      `json:"accent_color,omitempty"`
 	PrimaryColor                  string      `json:"primary_color,omitempty"`
 	MediaType                     string      `json:"media_type,omitempty"`
