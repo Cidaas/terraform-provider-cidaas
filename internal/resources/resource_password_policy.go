ed24e41d5be705d2dfbdd04cc2b859e558c7bf0d
Tujit Bora <tujit.bora@widas.in>
2024-08-06 13:45:53 +0530

    password policy documentation

--- internal/resources/resource_password_policy.go
+++ internal/resources/resource_password_policy.go
@@ -58,13 +58,11 @@ func (r *PasswordPolicy) ModifyPlan(ctx context.Context, req resource.ModifyPlan
 	if req.Plan.Raw.IsNull() {
 		return
 	}
-
 	var plan PasswordPolicyConfig
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
-
 	if plan.ID.IsUnknown() {
 		resp.Diagnostics.AddError(
 			"Resource Creation Not Allowed",
@@ -72,50 +70,69 @@ func (r *PasswordPolicy) ModifyPlan(ctx context.Context, req resource.ModifyPlan
 		)
 	}
 }
+
 func (r *PasswordPolicy) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		MarkdownDescription: "The Password Policy resource in the provider allows you to manage the password policy within the Cidaas." +
+			"\nNote that resource creation is not allowed, only updates are permitted after the resource has been imported." +
+			"\n\n Ensure that the below scopes are assigned to the client with the specified `client_id`:" +
+			"\n- cidaas:password_policy_read" +
+			"\n- cidaas:password_policy_write",
 		Attributes: map[string]schema.Attribute{
 			// The id is used to determine the operation type.
 			// If id is unknown, it indicates a create operation. Otherwise, it indicates an update operation.
 			// Note: Creation is not allowed, only updates are permitted after importing the resource.
 			"id": schema.StringAttribute{
 				Computed: true,
+				MarkdownDescription: "Unique identifier of the password policy. This will be set to the same value as the import identifier." +
+					"\nWhile the cidaas API does not require an identifier to import password policy, Terraform's import command does. Therefore, you can provide any arbitrary string as the identifier.",
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
 			},
 			"maximum_length": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The maximum length allowed for the password. The `maximum_length` must be greater than `minimum_length`",
+				Validators: []validator.Int64{
+					int64validator.AtLeastSumOf(path.MatchRoot("minimum_length")),
+				},
 			},
 			"minimum_length": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The minimum length required for the password. The `minimum_length` must be greater than or equal to the sum of `no_of_special_chars`, `no_of_digits`, and `lowercase/uppercase` characters.",
 			},
 			"no_of_special_chars": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The required number of special characters in the password.",
 			},
 			"no_of_digits": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The required number of digits in the password.",
 			},
 			"lower_and_uppercase": schema.BoolAttribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "Specifies whether the password must contain both lowercase and uppercase letters.",
 			},
 			"reuse_limit": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The number of previous passwords that cannot be reused. This number cannot exceed 5.",
 				Validators: []validator.Int64{
 					int64validator.AtMost(5),
 				},
 			},
 			"expiration_in_days": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The number of days after which the password expires.",
 			},
 			"no_of_days_to_remind": schema.Int64Attribute{
-				Required: true,
+				Required:            true,
+				MarkdownDescription: "The number of days before the password expiration to remind the user to change their password.",
 			},
 		},
 	}
 }
 
-func (r *PasswordPolicy) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+func (r *PasswordPolicy) Create(_ context.Context, _ resource.CreateRequest, resp *resource.CreateResponse) {
 	resp.Diagnostics.AddError(
 		"Resource Creation Not Allowed",
 		"Creating this resource using 'terraform apply' is not allowed. You must first import the existing resource using 'terraform import'. After the import, you can perform updates as needed.",
@@ -170,7 +187,7 @@ func (r *PasswordPolicy) Update(ctx context.Context, req resource.UpdateRequest,
 	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
 }
 
-func (r *PasswordPolicy) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+func (r *PasswordPolicy) Delete(_ context.Context, _ resource.DeleteRequest, _ *resource.DeleteResponse) {
 }
 
 func (r *PasswordPolicy) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
